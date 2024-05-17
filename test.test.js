0e9411cfc76110687b8ca272523245479948eb52
Afonichev Ilya <afonichev007@gmail.com>
2024-05-17 12:44:42 +0300

    commit

--- test.test.js
+++ test.test.js
@@ -7,7 +7,9 @@ describe("Functional Testing with Selenium WebDriver", () => {
     driver = await new Builder().forBrowser("MicrosoftEdge").build();
   });
 
-  afterAll(async () => {});
+  afterAll(async () => {
+    await driver.quit();
+  });
 
   it("Тестируем результат открытия страницы, что заголовок сайта равен указанной строке", async () => {
     await driver.get("https://www.wildberries.ru");
@@ -15,21 +17,21 @@ describe("Functional Testing with Selenium WebDriver", () => {
       until.titleIs(
         "Интернет-магазин Wildberries: широкий ассортимент товаров - скидки каждый день!"
       ),
-      3000
+      10000
     );
     const title = await driver.getTitle();
     expect(title).toMatch(
       "Интернет-магазин Wildberries: широкий ассортимент товаров - скидки каждый день!"
     );
-  }, 40000);
+  }, 15000);
 
   it("Проверка видимости объектов", async () => {
     await driver.get("https://www.wildberries.ru/");
-    driver.sleep(3000);
 
     const element = await driver.findElement(
       By.xpath("/html/body/div[1]/header")
     );
+    await driver.wait(until.elementIsVisible(element), 10000);
     const isDisplayed = await element.isDisplayed();
     expect(isDisplayed).toBe(true);
   });
@@ -45,31 +47,40 @@ describe("Functional Testing with Selenium WebDriver", () => {
       10000
     );
     const url = await driver.getCurrentUrl();
-    expect(url).toContain("");
+    expect(url).toContain("https://www.wildberries.ru/lk/basket");
   });
+
   it("Эмуляция нажатия на кнопку", async () => {
     await driver.get("https://www.wildberries.ru/");
     const button = await driver.findElement(By.xpath("/html/body/div[2]/a[3]"));
-
     await button.click();
 
-    driver.sleep(3000);
     const result = await driver.findElement(
       By.xpath("/html/body/div[1]/div/div")
     );
-    driver.sleep(3000);
-
+    await driver.wait(until.elementIsVisible(result), 10000);
     const isDisplayed = await result.isDisplayed();
     expect(isDisplayed).toBe(true);
   });
+
   it("Заполнение текстового поля", async () => {
     await driver.get("https://www.wildberries.ru/");
 
-    const input = await driver.findElement(
-      By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/div[1]/input")
+    const input = await driver.wait(
+      until.elementLocated(
+        By.xpath("/html/body/div[1]/header/div/div[2]/div[2]/div[1]/input")
+      ),
+      15000
     );
+
     await input.sendKeys("тест");
 
+    await driver.wait(async function () {
+      const value = await input.getAttribute("value");
+      return value === "тест";
+    }, 10000);
+
+    // Проверяем значение
     const value = await input.getAttribute("value");
     expect(value).toBe("тест");
   }, 20000);
